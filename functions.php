878984f8cc2956eb5a4faf313e4075f38c75f6fe
furPedroRibeiro <84580101+furPedroRibeiro@users.noreply.github.com>
2023-05-04 15:52:26 -0300

    site projeto - em desenvolvimento
    home pronto(responsividade ainda pendente)

--- functions.php
+++ functions.php
@@ -33,10 +33,55 @@
     $res = $GLOBALS['conn']->query($sql);
 
     if($res->num_rows > 0){
-      messageRefreshHome("Login efetuado com sucesso");
+      while($row = $res->fetch_assoc()){
+        echo $row['rm'];
+        echo $row['nm_usuario'];
+        $rm_num = $row['rm'];
+        // messageRefreshHome("Login efetuado com sucesso");
+      } 
     } else{
-      messageRefreshHome("Erro ao fazer login");
+      messageRefreshIndex("Erro ao fazer login");
     }
+    echo $GLOBALS['rm']->rm;
+
   }
 
+  function formInscricaoEquipe($modalidade, $rm, $nm){
+    $sql = 'SELECT * FROM equipe WHERE id_modalidade ='.$modalidade;
+    $res = $GLOBALS['conn']->query($sql);
+
+    echo '
+      <form action="" method="post" class="form">
+          <!-- SELECT DAS EQUIPES -->
+              <label for="select-equipe">Selecione sua equipe</label>
+              <select name="select-equipe" id="select-equipe">
+    ';
+    if($res->num_rows > 0){
+      while($row = $res->fetch_assoc()){
+        echo '
+                <option value="'.$row['id'].'">'.$row['nm_equipe'].'</option>
+            ';
+      } 
+    } else{
+    }
+
+    
+    echo '
+        </select>
+        <!-- FIM SELECT DAS EQUIPES -->
+        <!-- RM USUARIO -->
+            <label for="rm-usuario">Seu rm</label>
+            <input type="text" id="rm-usuario" name="rm-usuario" disabled value="';
+    echo
+    echo    '">
+        <!-- FIM RM USUARIO -->
+        <!-- NOME USUARIO -->
+            <label for="nome-usuario">Seu nome</label>
+            <input type="text" id="nome-usuario" name="nome-usuario" disabled value="'.$nm.'">
+        <!-- FIM NOME USUARIO -->
+        <button type="submit" class="btn">Enviar</button>
+    </form>
+    ';
+    
+  }
 ?>
--- home/script.js
+++ home/script.js
@@ -1,32 +1,37 @@
 function showFutsal(){
     document.getElementById("title-page-table").textContent = "Futsal - Tabela";
-    document.getElementById("img-table").src = "../assets/chaveamentos/chaveamento_futsal.jpg";
+    document.getElementById("img-table").src = "../assets/chaveamentos/chaveamento_futsal.png";
 }
 function showBasquete(){
     document.getElementById("title-page-table").textContent = "Basquete - Tabela";
-    document.getElementById("img-table").src = "../assets/chaveamentos/chaveamento_basquete.jpg";
+    document.getElementById("img-table").src = "../assets/chaveamentos/chaveamento_basquete.png";
 }
 function showVolei(){
     document.getElementById("title-page-table").textContent = "Vôlei - Tabela";
-    document.getElementById("img-table").src = "../assets/chaveamentos/chaveamento_volei.jpg";
+    document.getElementById("img-table").src = "../assets/chaveamentos/chaveamento_volei.png";
 }
 function showTenisdemesa(){
     document.getElementById("title-page-table").textContent = "Tênis de Mesa - Tabela";
-    document.getElementById("img-table").src = "../assets/chaveamentos/chaveamento_tenisdemesa.jpg";
+    document.getElementById("img-table").src = "../assets/chaveamentos/chaveamento_tenisdemesa.png";
 }
 function showCsgo(){
     document.getElementById("title-page-table").textContent = "CS:GO - Tabela";
-    document.getElementById("img-table").src = "../assets/chaveamentos/chaveamento_csgo.jpg";
+    document.getElementById("img-table").src = "../assets/chaveamentos/chaveamento_csgo.png";
 }
 function showValorant(){
     document.getElementById("title-page-table").textContent = "Valorant - Tabela";
-    document.getElementById("img-table").src = "../assets/chaveamentos/chaveamento_valorant.jpg";
+    document.getElementById("img-table").src = "../assets/chaveamentos/chaveamento_valorant.png";
 }
 function showLol(){
     document.getElementById("title-page-table").textContent = "League Of Legends - Tabela";
-    document.getElementById("img-table").src = "../assets/chaveamentos/chaveamento_lol.jpg";
+    document.getElementById("img-table").src = "../assets/chaveamentos/chaveamento_lol.png";
 }
 function showFifa(){
     document.getElementById("title-page-table").textContent = "FIFA - Tabela";
-    document.getElementById("img-table").src = "../assets/chaveamentos/chaveamento_fifa.jpg";
+    document.getElementById("img-table").src = "../assets/chaveamentos/chaveamento_fifa.png";
 }
+
+function showBasqueteInfo(){
+    document.getElementsById("#verBasquete").style.display = 'none';
+    document.getElementsById("#imgBasquete").style.width = 'none';
+} 
