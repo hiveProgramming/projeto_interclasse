37456cb53f18103438c37c7a320517b949ba3312
furPedroRibeiro <84580101+furPedroRibeiro@users.noreply.github.com>
2023-05-04 00:25:46 -0300

    Projeto pronto - front-end

---
+++ home/script.js
@@ -0,0 +1,32 @@
+function showFutsal(){
+    document.getElementById("title-page-table").textContent = "Futsal - Tabela";
+    document.getElementById("img-table").src = "../assets/chaveamentos/chaveamento_futsal.jpg";
+}
+function showBasquete(){
+    document.getElementById("title-page-table").textContent = "Basquete - Tabela";
+    document.getElementById("img-table").src = "../assets/chaveamentos/chaveamento_basquete.jpg";
+}
+function showVolei(){
+    document.getElementById("title-page-table").textContent = "Vôlei - Tabela";
+    document.getElementById("img-table").src = "../assets/chaveamentos/chaveamento_volei.jpg";
+}
+function showTenisdemesa(){
+    document.getElementById("title-page-table").textContent = "Tênis de Mesa - Tabela";
+    document.getElementById("img-table").src = "../assets/chaveamentos/chaveamento_tenisdemesa.jpg";
+}
+function showCsgo(){
+    document.getElementById("title-page-table").textContent = "CS:GO - Tabela";
+    document.getElementById("img-table").src = "../assets/chaveamentos/chaveamento_csgo.jpg";
+}
+function showValorant(){
+    document.getElementById("title-page-table").textContent = "Valorant - Tabela";
+    document.getElementById("img-table").src = "../assets/chaveamentos/chaveamento_valorant.jpg";
+}
+function showLol(){
+    document.getElementById("title-page-table").textContent = "League Of Legends - Tabela";
+    document.getElementById("img-table").src = "../assets/chaveamentos/chaveamento_lol.jpg";
+}
+function showFifa(){
+    document.getElementById("title-page-table").textContent = "FIFA - Tabela";
+    document.getElementById("img-table").src = "../assets/chaveamentos/chaveamento_fifa.jpg";
+}
